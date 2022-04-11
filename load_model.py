c538802872c9d4ec6875f46c9700ded15c1814ff
yyyyy98 <103376430+yyyyy98@users.noreply.github.com>
2022-04-11 14:21:46 +0800

    Create load_model.py

---
+++ load_model.py
@@ -0,0 +1,11 @@
+import numpy as np
+import model
+import dataloader
+net = model.TwoLayerNet(784, 300, 10)
+net.params['W1'] = np.load('./trained_model.npz')['W1']
+net.params['b1'] = np.load('./trained_model.npz')['b1']
+net.params['W2'] = np.load('./trained_model.npz')['W2']
+net.params['b2'] = np.load('./trained_model.npz')['b2']
+
+test_x, test_y = dataloader.load_mnist('./mnist', 't10k')
+np.mean(test_y == net.predict(test_x))
