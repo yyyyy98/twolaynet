9b3a7bacff5d9c5d82321233b5dc72b79bd31231
yyyyy98 <103376430+yyyyy98@users.noreply.github.com>
2022-04-11 14:52:30 +0800

    Update param_selection.py

--- param_selection.py
+++ param_selection.py
@@ -4,8 +4,10 @@ import os
 import struct
 import dataloader
 
+# store best accuracy
 best_acc = 0
 best_stats = None
+# hyperparameter lists
 learning_rates = [1e-2, 5e-2, 1e-3, 5e-3]
 reg_strengths = [1e-2, 5e-2, 1e-3, 5e-3]
 hidden_sizes = [100, 300, 500]
@@ -15,8 +17,10 @@ input_size = 28 * 28
 num_class = 10
 batch_size = 200
 data, label = dataloader.load_mnist('./mnist')
+# split train into train and validation
 train_x, train_y = data[0:50000, :], label[0:50000]
 val_x, val_y = data[50000:, :], label[50000:]
+# tune hyperparameters
 for lr in learning_rates:
     for rs in reg_strengths:
         for hs in hidden_sizes:            
@@ -27,6 +31,7 @@ for lr in learning_rates:
             val_y_pred = net.predict(val_x)
             val_acc = np.mean(val_y == val_y_pred)
             results[(lr,rs,hs)] = (train_acc, val_acc)
+            # update best results
             if best_acc < val_acc:
                 best_stats = stats
                 best_acc = val_acc
